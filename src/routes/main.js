24cc3ad34b93778f14d4588c3ce315fb19a40d0e
mindkeeper <btjjr23@gmail.com>
2022-12-02 18:03:32 +0700

    adding movie routes

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mainRouter = express.Router();
 const auth = require("./auth");
 const profile = require("./user");
-
+const movie = require("./movies");
 const prefix = "/api";
 
 mainRouter.use(`${prefix}/auth`, auth);
 mainRouter.use(`${prefix}/user`, profile);
-
+mainRouter.use(`${prefix}/movie`, movie);
 mainRouter.get(`/`, (req, res) => {
   res.json({ msg: "Welcome" });
 });
