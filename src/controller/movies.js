547e8d02b00c269e82cc625cb5cafcf949fe24f9
mindkeeper <btjjr23@gmail.com>
2022-12-02 18:02:37 +0700

    create movie undone

---
+++ src/controller/movies.js
@@ -0,0 +1,29 @@
+const resHelper = require("../helper/sendResponse");
+const movieRepo = require("../model/movies");
+
+const createMovie = async (req, res) => {
+  try {
+    console.log(req.body);
+    const createMovie = await movieRepo.createMovie(req);
+    const createMovieGenre = await movieRepo.createMovieGenre(
+      req.body.genreId,
+      createMovie.id
+    );
+    const showTime = await movieRepo.createMovieShowTime(
+      req.body.cinemasLocationsId,
+      createMovie.id,
+      req.body.showDate
+    );
+    resHelper.success(res, 200, {
+      data: showTime.data,
+      msg: "Success",
+    });
+  } catch (error) {
+    console.log(error);
+    resHelper.error(res, error.status, error);
+  }
+};
+
+const movieController = { createMovie };
+
+module.exports = movieController;
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -2,5 +2,29 @@ const moviesRouter = require("express").Router();
 const allowedRoles = require("../middleware/allowedRoles");
 const cloudinary = require("../middleware/cloudinary");
 const validate = require("../middleware/validate");
+const upload = require("../middleware/uploadSingle");
+const isLogin = require("../middleware/isLogin");
+const movieController = require("../controller/movies");
 
+const newMoviesBody = [
+  "movieName",
+  "synopsis",
+  "releaseDate",
+  "duration",
+  "director",
+  "price",
+  "genreId",
+  "cinemasLocationsId",
+  "showDate",
+];
+
+moviesRouter.post(
+  "/new",
+  isLogin(),
+  allowedRoles("admin"),
+  upload,
+  cloudinary,
+  validate.body(...newMoviesBody),
+  movieController.createMovie
+);
 module.exports = moviesRouter;
