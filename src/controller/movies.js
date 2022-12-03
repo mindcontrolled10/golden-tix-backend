fc9b8bcaf1e63ddf3c5c9b79623df6744d083281
mindkeeper <btjjr23@gmail.com>
2022-12-03 14:26:10 +0700

    add upcoming movies

--- src/controller/movies.js
+++ src/controller/movies.js
@@ -3,28 +3,42 @@ const movieRepo = require("../model/movies");
 
 const createMovie = async (req, res) => {
   try {
-    console.log(req.body);
     const createMovie = await movieRepo.createMovie(req);
     const createMovieGenre = await movieRepo.createMovieGenre(
       req.body.genreId,
       createMovie.id
     );
+    const createMovieCast = await movieRepo.createMovieCast(
+      req.body.castIds,
+      createMovie.id
+    );
     const showTime = await movieRepo.createMovieShowTime(
       req.body.cinemasLocationsId,
       createMovie.id,
-      req.body.showDate,
-      req.body.schedules
+      req.body.showDate
+    );
+    console.log(showTime.data.ids);
+    const createSchedule = await movieRepo.createScheduleMovie(
+      showTime.data.ids,
+      req.body.schedulesId
     );
     resHelper.success(res, 200, {
-      data: showTime.data,
-      msg: "Success",
+      msg: "Create Movie Success",
     });
   } catch (error) {
     console.log(error);
     resHelper.error(res, error.status, error);
   }
 };
+const getUpcomingMovies = async (req, res) => {
+  try {
+    const response = await movieRepo.getUpcomingMovies(req);
+    resHelper.success(res, response.status, response);
+  } catch (error) {
+    resHelper.error(res, error.status, error);
+  }
+};
 
-const movieController = { createMovie };
+const movieController = { createMovie, getUpcomingMovies };
 
 module.exports = movieController;
