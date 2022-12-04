36d475f8d475de0cb58ced28d370cb9e5d399961
mindkeeper <btjjr23@gmail.com>
2022-12-04 16:05:35 +0700

    upload cloudinary

--- src/middleware/cloudinary.js
+++ src/middleware/cloudinary.js
@@ -20,7 +20,6 @@ const uploaderCloudinary = async (req, res, next) => {
       clodinaryOpt
     );
     req.file = `${result.public_id}.${result.format}`;
-    console.log(req.file);
     next();
   } catch (error) {
     console.log(error);
